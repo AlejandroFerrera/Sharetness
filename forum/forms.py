58347511c2f2fa96454515524c475b170b7f6ec3
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-06-22 01:09:37 -0400

    added time_to_delete function

--- forum/forms.py
+++ forum/forms.py
@@ -1,10 +1,19 @@
 from django.forms import ModelForm
 from .models import Room
+from django.utils.translation import gettext_lazy as _
 
 class RoomForm(ModelForm):
     
     class Meta:
         model = Room
         fields = '__all__'
+        
+        labels = {
+            'topic': _('Tema'),
+            'name': _('Nombre'),
+            'description': _('Descripción'),
+        }
+
+        exclude = ['host']
 
 
--- forum/views.py
+++ forum/views.py
@@ -85,7 +85,10 @@ def create_room(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect("home")
 
     context = {'form': form}
